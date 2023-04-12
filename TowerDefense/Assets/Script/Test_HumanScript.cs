03625e6215a810c31c735f665e505703a815eec2
Nishi0017 <c2216@st.cs.kumamoto-u.ac.jp>
2023-04-12 17:11:49 +0900

    NavMeshPro

---
+++ TowerDefense/Assets/Script/Test_HumanScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Test_HumanScript : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    public GameObject startPosition;
+    public GameObject goalPosition;
+    private bool isStart = false;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        transform.position = startPosition.transform.position;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            isStart = !isStart;
+            animator.SetBool("walk_bool", isStart);
+        }
+        if(isStart)
+        {
+            agent.destination = goalPosition.transform.position;
+        }
+    }
+}
