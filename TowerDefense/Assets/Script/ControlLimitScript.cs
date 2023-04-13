7ea39f8d5cd267cc24588a10a3b8ddb4459a0b27
Nishi0017 <c2216@st.cs.kumamoto-u.ac.jp>
2023-04-13 12:13:23 +0900

    Port

---
+++ TowerDefense/Assets/Script/ControlLimitScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlLimitScript : MonoBehaviour
+{
+    [SerializeField] private Vector3 pos_min;
+    [SerializeField] private Vector3 pos_max;
+    private Transform target;
+    private Vector3 pos;
+
+    private void Update()
+    {
+        transform.position = target.position;
+        pos.x = Mathf.Clamp(this.transform.localPosition.x, pos_min.x, pos_max.x);
+        pos.y = Mathf.Clamp(this.transform.localPosition.y, pos_min.y, pos_max.y);
+        pos.z = Mathf.Clamp(this.transform.localPosition.z, pos_min.z, pos_max.z);
+        transform.localPosition = pos;
+    }
+}
