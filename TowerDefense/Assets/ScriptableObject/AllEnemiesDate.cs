d731b0f6af76631d9720cf6a9ab3555b1d963b6f
Nishi0017 <c2216@st.cs.kumamoto-u.ac.jp>
2023-07-03 19:57:43 +0900

    delete

--- TowerDefense/Assets/ScriptableObject/AllEnemiesDate.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AllEnemyDate", menuName = "ScriptableObject/All Enemy Date")]
-public class AllEnemiesDate : ScriptableObject
-{
-    public EnemyDate[] enemiesDate;
-}
