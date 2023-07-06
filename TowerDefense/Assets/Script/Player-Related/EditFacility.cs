b5d560a9cd32eb26da06601aed2793c90ecfefe4
Nishi0017 <c2216@st.cs.kumamoto-u.ac.jp>
2023-07-06 23:44:42 +0900

    防衛施設設置

--- TowerDefense/Assets/Script/Player-Related/EditFacility.cs
+++ TowerDefense/Assets/Script/Player-Related/EditFacility.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class EditFacility : MonoBehaviour
@@ -15,8 +16,35 @@ public class EditFacility : MonoBehaviour
     //無視するレイヤーマスク
     private LayerMask ignoreLayers;
 
+    //Stateの種類
+    public enum EditState
+    {
+        None,
+        Put,
+        Delete,
+        LevelUp
+    }
+    private EditState currentState; //現在のステート
+    private bool stateEnter = false; //ステート変更してからⅠ回目のフレームであることを表す
+    private float stateTime = 0.0f; //ステートに移行してからの時間を保存
+
+    /// <summary>
+    /// ステート移行関数
+    /// </summary>
+    /// <param name="newState"></param>
+    public void ChangeState(EditState newState)
+    {
+        currentState = newState;
+        stateEnter = true;
+        stateTime = 0.0f;
+        Debug.Log("現在のステート" + currentState);
+    }
+
     private void Start()
     {
+        currentState = EditState.None;
+        Debug.Log("現在のステート" + currentState);
+
         // Player Layer, Enemy Layer, Defense Layerを無視するレイヤーマスクを作成
         int playerLayer = LayerMask.NameToLayer("PlayerLayer");
         int enemyLayer = LayerMask.NameToLayer("EnemyLayer");
@@ -26,42 +54,89 @@ public class EditFacility : MonoBehaviour
 
     private void Update()
     {
-        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        stateTime = Time.deltaTime;
+
+        switch (currentState)
         {
-            RaycastHit hit;
+            case EditState.None:
+                
+                break;
 
-            Vector3 rayOrigin = rightController.transform.position;
-            Vector3 rayDirection = rightController.transform.forward;
+            case EditState.Put:
+                
+                break;
+            
+            case EditState.Delete:
+                break;
 
-            if (Physics.Raycast(rayOrigin, rayDirection, out hit, Mathf.Infinity, ignoreLayers))
-            {
-                createdObject = Instantiate(createObject, hit.point, Quaternion.identity);
-                isObjectMoving = true;
-            }
+            case EditState.LevelUp:
+                break;
         }
 
-        if (OVRInput.Get(OVRInput.RawButton.RIndexTrigger))
+
+
+
+
+    }
+
+    private void UpdatePutState()
+    {
+        //施設生成待ち
+        if (!isObjectMoving)
         {
+            //施設の生成
+            if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+            {
+                RaycastHit hit;
+
+                Vector3 rayOrigin = rightController.transform.position;
+                Vector3 rayDirection = rightController.transform.forward;
+
+                if (Physics.Raycast(rayOrigin, rayDirection, out hit, Mathf.Infinity, ignoreLayers))
+                {
+                    createdObject = Instantiate(createObject, hit.point, Quaternion.identity);
+                    isObjectMoving = true;
+                }
+
+            }
+        }
+        else
+        {
+            //施設の移動
             RaycastHit hit;
 
             Vector3 rayOrigin = rightController.transform.position;
             Vector3 rayDirection = rightController.transform.forward;
-
+            
             if (Physics.Raycast(rayOrigin, rayDirection, out hit, Mathf.Infinity, ignoreLayers))
             {
                 createdObject.transform.position = hit.point;
             }
+
+            //施設の生成キャンセル
+            if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+            {
+                Destroy(createObject);
+                isObjectMoving = false;
+            }
+
+            //施設の設置確定
+            if (OVRInput.GetDown(OVRInput.RawButton.A))
+            {
+                isObjectMoving = false;
+            }
         }
 
-        if(OVRInput.GetUp(OVRInput.RawButton.RIndexTrigger))
-        {
-            isObjectMoving= false;
-        }
 
-        if(isObjectMoving)
-        {
+    }
 
-        }
+    private void UpdateDeleteState()
+    {
+
+    }
+
+    private void UpdateLevelUpState()
+    {
 
     }
 }
